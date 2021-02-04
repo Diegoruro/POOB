0b9a68313a44b377c4eaab53c01718e114662c51
Juan Felipe Aguas Pulido <aguasfelipe02@gmail.com>
2021-02-04 13:02:36 -0500

    Add code from shapes and first implement of Dice

---
+++ Dice+/Dice.java
@@ -0,0 +1,159 @@
+import java.util.Random;
+import java.awt.*;
+
+/**
+ * Primera clase POOB.
+ * 
+ * @author (Juan Felipe Aguas Pulido - Diego Fernando Ruiz Rojas) 
+ */
+public class Dice
+{
+    private int value;
+    private boolean isVisible;
+    
+    private static final int size = 150;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private Rectangle sq;
+    
+    private Circle[] points = new Circle[6];
+    
+    
+
+    /**
+     * Constructor for objects of class Dice
+     */
+    public Dice()
+    {
+        this.xPosition = 75;
+        this.yPosition = 75;
+        this.isVisible = false;
+        
+    }
+
+
+    public int getValue()
+    {
+        return this.value;
+    }
+    
+
+    public void moveHorizontal(int distance)
+    {
+       delete(sq);
+       xPosition += distance;
+       draw();
+    }
+    
+
+    public void roll()
+    {
+       Random n = new Random();
+       this.value = n.nextInt(6) + 1;
+       delete(sq);
+       this.draw();
+    }
+    
+    public void makeVisible()
+    {
+           isVisible = true;
+           draw();
+    }
+    
+
+    public void makeInvisible()
+    {
+           delete(sq);
+           isVisible = false;
+    }
+    
+    
+
+    private void draw()
+    {
+        if(isVisible) {
+            switch(this.value){
+                case 1:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 60,yPosition + 60,30);
+                    points[0].makeVisible();
+                    break;
+                case 2:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 60,yPosition + 30,30);
+                    points[0].makeVisible();
+                    points[1] = new Circle(xPosition + 60,yPosition + 90,30);
+                    points[1].makeVisible();
+                    break;
+                case 3:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 15,yPosition + 15,30);
+                    points[0].makeVisible();
+                    points[1] = new Circle(xPosition + 60,yPosition + 60,30);
+                    points[1].makeVisible();
+                    points[2] = new Circle(xPosition + 105,yPosition + 105,30);
+                    points[2].makeVisible();
+                    break;
+                case 4:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 30,yPosition + 90,30);
+                    points[0].makeVisible();
+                    points[1] = new Circle(xPosition + 90,yPosition + 30,30);
+                    points[1].makeVisible();
+                    points[2] = new Circle(xPosition + 90,yPosition + 90,30);
+                    points[2].makeVisible();
+                    points[3] = new Circle(xPosition + 30,yPosition + 30,30);
+                    points[3].makeVisible();
+                    break;
+                case 5:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 30,yPosition + 90,30);
+                    points[0].makeVisible();
+                    points[1] = new Circle(xPosition + 90,yPosition + 30,30);
+                    points[1].makeVisible();
+                    points[2] = new Circle(xPosition + 90,yPosition + 90,30);
+                    points[2].makeVisible();
+                    points[3] = new Circle(xPosition + 30,yPosition + 30,30);
+                    points[3].makeVisible();
+                    points[4] = new Circle(xPosition + 60,yPosition + 60,30);
+                    points[4].makeVisible();
+                    break;
+                case 6:
+                    sq = new Rectangle(xPosition, yPosition, size, size);
+                    sq.makeVisible();
+                    points[0] = new Circle(xPosition + 30,yPosition + 15,30);
+                    points[0].makeVisible();
+                    points[1] = new Circle(xPosition + 30,yPosition + 60,30);
+                    points[1].makeVisible();
+                    points[2] = new Circle(xPosition + 30,yPosition + 105,30);
+                    points[2].makeVisible();
+                    points[3] = new Circle(xPosition + 90,yPosition + 15,30);
+                    points[3].makeVisible();
+                    points[4] = new Circle(xPosition + 90,yPosition + 60,30);
+                    points[4].makeVisible();
+                    points[5] = new Circle(xPosition + 90,yPosition + 105,30);
+                    points[5].makeVisible();
+                    break;
+                default:
+                    System.out.println("Plase roll the dice");
+                    break;
+                    
+            }
+        }
+    }
+    
+    
+    private void delete(Rectangle cuadro)
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(cuadro);
+        }
+    }
+}
---
+++ Dice+/Rectangle.java
@@ -0,0 +1,250 @@
+import java.awt.*;
+
+/**
+ * A rectangle that can be manipulated and that draws itself on a canvas.
+ * 
+ * @author  Michael Kolling and David J. Barnes (Modified)
+ * @version 1.0  (15 July 2000)()
+ */
+
+
+ 
+public class Rectangle{
+
+    public static int EDGES = 4;
+    
+    private int height;
+    private int width;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private boolean isVisible;
+
+    /**
+     * Create a new rectangle at default position with default color.
+     */
+    public Rectangle(int x, int y, int h, int w){
+        height = h;
+        width = w;
+        xPosition = x;
+        yPosition = y;
+        color = "red";
+        isVisible = false;
+    }
+    
+
+    /**
+     * Make this rectangle visible. If it was already visible, do nothing.
+     */
+    public void makeVisible(){
+        isVisible = true;
+        draw();
+    }
+    
+    /**
+     * Make this rectangle invisible. If it was already invisible, do nothing.
+     */
+    public void makeInvisible(){
+        erase();
+        isVisible = false;
+    }
+    
+    /**
+     * Move the rectangle a few pixels to the right.
+     */
+    public void moveRight(){
+        moveHorizontal(20);
+    }
+
+    /**
+     * Move the rectangle a few pixels to the left.
+     */
+    public void moveLeft(){
+        moveHorizontal(-20);
+    }
+
+    /**
+     * Move the rectangle a few pixels up.
+     */
+    public void moveUp(){
+        moveVertical(-20);
+    }
+
+    /**
+     * Move the rectangle a few pixels down.
+     */
+    public void moveDown(){
+        moveVertical(20);
+    }
+
+    /**
+     * Move the rectangle horizontally.
+     * @param distance the desired distance in pixels
+     */
+    public void moveHorizontal(int distance){
+        erase();
+        xPosition += distance;
+        draw();
+    }
+
+    /**
+     * Move the rectangle vertically.
+     * @param distance the desired distance in pixels
+     */
+    public void moveVertical(int distance){
+        erase();
+        yPosition += distance;
+        draw();
+    }
+
+    /**
+     * Slowly move the rectangle horizontally.
+     * @param distance the desired distance in pixels
+     */
+    public void slowMoveHorizontal(int distance){
+        int delta;
+
+        if(distance < 0) {
+            delta = -1;
+            distance = -distance;
+        } else {
+            delta = 1;
+        }
+
+        for(int i = 0; i < distance; i++){
+            xPosition += delta;
+            draw();
+        }
+    }
+
+    /**
+     * Slowly move the rectangle vertically.
+     * @param distance the desired distance in pixels
+     */
+    public void slowMoveVertical(int distance){
+        int delta;
+
+        if(distance < 0) {
+            delta = -1;
+            distance = -distance;
+        } else {
+            delta = 1;
+        }
+
+        for(int i = 0; i < distance; i++){
+            yPosition += delta;
+            draw();
+        }
+    }
+
+    /**
+     * Change the size to the new size
+     * @param newHeight the new height in pixels. newHeight must be >=0.
+     * @param newWidht the new width in pixels. newWidth must be >=0.
+     */
+    public void changeSize(int newHeight, int newWidth) {
+        erase();
+        height = newHeight;
+        width = newWidth;
+        draw();
+    }
+    
+    /**
+     * Change the color. 
+     * @param color the new color. Valid colors are "red", "yellow", "blue", "green",
+     * "magenta" and "black".
+     */
+    public void changeColor(String newColor){
+        color = newColor;
+        draw();
+    }
+
+    /*
+     * Draw the rectangle with current specifications on screen.
+     */
+
+    private void draw() {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.draw(this, color,
+                new java.awt.Rectangle(xPosition, yPosition, 
+                                       width, height));
+            canvas.wait(50);
+        }
+    }
+
+    /*
+     * Erase the rectangle on screen.
+     */
+    private void erase(){
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    //{"red", "blue", "yellow", "black"}
+    public void rainbow(String[] colors)
+    {
+        for (String color : colors){
+            this.changeColor(color);
+            draw();
+        }
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public void rotate()
+    {
+       int aux1, aux2;
+       aux1 = this.height;
+       aux2 = this.width;
+       this.width = aux1;
+       this.height = aux2;
+       draw();
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public int area()
+    {
+        return this.height * this.width;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public void zoom(char c)
+    {
+        if (c == '-'){
+            this.height -= this.height*0.10;
+            this.width -= this.width*0.10;
+        }else{
+            this.height += this.height*0.10;
+            this.width += this.width*0.10;
+        }
+        draw();
+    }
+
+
+
+}
+
