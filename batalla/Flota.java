a969aea2c758099904d233a3686fe2cd6df20f45
Juan Felipe Aguas Pulido <aguasfelipe02@gmail.com>
2021-02-15 17:35:37 -0500

    Avance Batalla

--- batalla/Flota.java
+++ batalla/Flota.java
@@ -9,21 +9,41 @@ public class Flota {
     private ArrayList<Avion> aviones;
     private ArrayList<PortaAviones> portaAviones;
     private ArrayList<Marino> marinos;
+    public static ArrayList<Flota> flotas = new ArrayList<Flota>(2);
 
     public Flota(String nombre)
     {
         id = 123;
         minTrips = 500;
         score=new int[3];
+        barcos=new ArrayList<Barco>();
+        aviones = new ArrayList<Avion>();
+        marinos = new ArrayList<Marino>();
+        portaAviones = new ArrayList<PortaAviones>();
+        this.flotas.add(this);
+    }
+    
+    /**
+     * 
+     */
+    public void createBarco(int num)
+    {
+        Barco barco = new Barco(num);
+        barcos.add(barco);
         
     }
     
-    public void createBarco(int num)
+    
+    /**
+     * 
+     */
+    public void createAvion(String placa, boolean enAire)
     {
-        barcos=new ArrayList<Barco>();
-        barcos.add(new Barco(num));
+        Avion avion = new Avion(placa, enAire);
+        aviones.add(avion);
         
     }
+    
     
     /**
      * An example of a method - replace this comment with your own
@@ -37,6 +57,32 @@ public class Flota {
         {
             b.movePosition(deltaLongitud,deltaLatitud);
         }
+    }
+
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public  ArrayList<String> enAire()
+    {
+         Flota enemigo;
+         ArrayList<String> ans = new  ArrayList<String>();
+         if (this.nombre == this.flotas.get(0).nombre){
+             enemigo = this.flotas.get(1);
+         }else{
+             enemigo = this.flotas.get(0);
+         }
+         System.out.println(enemigo);
+         for (int i = 0; i < enemigo.aviones.size();i++){
+             System.out.println(enemigo.aviones.get(i).getEnAire());
+             if (enemigo.aviones.get(i).getEnAire()){
+                 ans.add(enemigo.aviones.get(i).getPlaca());
+             }
+         }
+         return ans;
     }
 
 }
