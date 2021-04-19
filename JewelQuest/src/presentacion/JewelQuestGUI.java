0236f0c27eca114a73742ca9a9a3205a4c2f2a6b
Diego Ruiz <diegofernandoruizrojas@gmail.com>
2021-04-19 16:19:39 -0500

    ciclo 3.1 en progreso x2

--- JewelQuest/src/presentacion/JewelQuestGUI.java
+++ JewelQuest/src/presentacion/JewelQuestGUI.java
@@ -3,12 +3,22 @@ package presentacion;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 
 public class JewelQuestGUI extends JFrame {
 
     JMenuItem nuevoMenu, abrirMenu, salvarMenu, salvarComoMenu, salirMenu;
+    private JPanel principal;
+    private JPanel ventanaTablero;
+    private JPanel opciones;
+    private CardLayout cd;
+    private JButton[][] tablero;
+    private int row, column;
 
     public JewelQuestGUI(){
+        cd = new CardLayout();
+        principal = new JPanel(cd);
+        add(principal);
         prepareElementos();
         prepareAcciones();
     }
@@ -27,13 +37,38 @@ public class JewelQuestGUI extends JFrame {
     }
 
     private void prepareAccionesMenu(){
+
+        nuevoMenu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                prepareElementosTablero();
+            }
+        });
         salirMenu.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 salir();
             }
         });
+        abrirMenu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrir(e);
+            }
+        });
+
+        salvarMenu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guardar(e);
+            }
+        });
     }
+
+    private void nuevo(){
+        cd.show(principal,"Tablero");
+    }
+
 
     //Metodos usados por PrepareAcciones
     private void salir(){
@@ -43,17 +78,50 @@ public class JewelQuestGUI extends JFrame {
         }
     }
 
-    private void prepareElementos(){
+    private void prepareElementos() {
         this.setTitle("Jewel Quest");
 
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) size.getWidth();
         int height = (int) size.getHeight();
-        this.setLocation(width/4,height/4);
+        this.setLocation(width / 4, height / 4);
 
-        this.setSize(width/2, height/2);
+        this.setSize(width / 2, height / 2);
 
         prepareElementosMenu();
+    }
+
+    private void prepareElementosTablero(){
+        ventanaTablero = new JPanel();
+        row=6;
+        column=6;
+        tablero = new JButton[row][column];
+        ventanaTablero.setLayout(null);
+        int heigth = ventanaTablero.getHeight()/row;
+        int width = ventanaTablero.getWidth()/column;
+        int x;
+        int y=0;
+        principal.add(ventanaTablero, "Tablero");
+        boolean intermitente=false;
+        for(int i=0;i<row;i++){
+            x=0;
+            for(int j=0;j<column;j++){
+                tablero[i][j]=new JButton();
+                tablero[i][j].setBounds(x,y,width,heigth);
+                if(intermitente){
+                    tablero[i][j].setBackground(Color.white);
+                    intermitente=false;
+                }
+                else{
+                    tablero[i][j].setBackground(Color.black);
+                    intermitente=true;
+                }
+                ventanaTablero.add(tablero[i][j]);
+                x+=heigth;
+            }
+            y+=width;
+        }
+        nuevo();
     }
 
     private void prepareElementosMenu(){
@@ -75,6 +143,29 @@ public class JewelQuestGUI extends JFrame {
         archivo.addSeparator();
         salirMenu = new JMenuItem("Salir");
         archivo.add(salirMenu);
+    }
+
+    private void abrir(java.awt.event.ActionEvent evt) {
+        JFileChooser fc = new JFileChooser();
+        int sel = fc.showOpenDialog(this);
+
+        if (sel==JFileChooser.APPROVE_OPTION){
+            File archivo = fc.getSelectedFile();
+            String nombre = archivo.getName();
+            JOptionPane.showMessageDialog(null,"Funcionalidad Abrir en construccion.\n Archivo a abrir : "+nombre);
+        }
+    }
+
+    private void guardar(java.awt.event.ActionEvent evt){
+        JFileChooser fc = new JFileChooser();
+        int sel =fc.showSaveDialog(this);
+
+        if (sel==JFileChooser.APPROVE_OPTION){
+            File archivo = fc.getSelectedFile();
+            String nombre = archivo.getName();
+            JOptionPane.showMessageDialog(null,"Funcionalidad Guardar en construccion.\n Archivo a guardar : "+nombre);
+
+        }
     }
 
 
